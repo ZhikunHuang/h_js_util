24e2446ae6d093d9360f81b443cac4a25750cd3b
BluckHuang <Bluck.Huang@grapecity.com>
2022-10-28 17:20:42 +0800

    add enum

--- src/Pub/PubConst.ts
+++ src/Pub/PubConst.ts
@@ -3,4 +3,8 @@ class PubConst{
      * mail address RegExp
      */
     static EmailReg : RegExp = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
+    /**
+     * pwd 数字、字母、特殊符号、大写字母
+     */
+    static PwdReg: RegExp = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/;
 }
